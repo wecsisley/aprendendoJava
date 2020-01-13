0fcf00252e31e4350bf00f18c5c0003595c296af
Wecsisley <wecsisley_carvalho@hotmail.com>
2020-01-12 22:31:56 -0300

    data de validade dinâmica, casa decimal acrescentada em precoDeCusto e precoDeVenda

--- clinica/src/pessoa/Medico.java
+++ clinica/src/pessoa/Medico.java
@@ -1,5 +1,10 @@
 package pessoa;
 
 public class Medico {
+	public static void main(String[] args) {
+		
+		String nome = "Lucas";
+		System.out.println(nome);
+	}
 
 }
--- exerciciosForaDoCurso/src/Comercio/CadastroDeProdutos.java
+++ exerciciosForaDoCurso/src/Comercio/CadastroDeProdutos.java
@@ -1,5 +1,7 @@
 package Comercio;
 
+import java.time.LocalDate;
+
 public class CadastroDeProdutos {
 	
 	public static void main(String[] args) {
@@ -12,8 +14,10 @@ public class CadastroDeProdutos {
 	    var grupo = "Laticínios";
 	    var vendidoPor = "unidade";
 	    var dataDeFabricacao = "12/12/2019";
-	    var dataDeVencimento = "12/02/2020";
-	    boolean verificacaoDeVencimento = false ;	    		
+	    var dataDeVencimento = "12/02/2020";	    
+	    LocalDate date1 = LocalDate.now();
+		LocalDate date2 = LocalDate.of(2020, 12, 02);
+		System.out.println();	    	    		
 	    var ncm = 04011010;
 	    var csosn = 102;
 	    var cfop = 5102;
@@ -21,17 +25,17 @@ public class CadastroDeProdutos {
 	    System.out.printf("\nProduto: %s \n\nCódigo de barras: %d \n\nPreço de Custo: R$ %.2f", 
 	    		produto, codigoDeBarras, precoDeCusto);
 	    
-	    System.out.println("\n\nPreço de Venda: R$ " + precoDeVenda + 0);
+	    System.out.printf("\n\nPreço de Venda: R$%.2f \n", precoDeVenda );
 	    
 	    System.out.printf("\nGrupo do produto: %s \n\nVendido por: %s \n\n",
 	    		grupo, vendidoPor);
 	    
-	    System.out.println("Lucro do produto: R$ " + lucro + 0);
+	    System.out.printf("Lucro do produto: R$%.2f \n ", lucro );
 	    
 	    System.out.printf("\nProduto fabricado em: %s \n\nVencimento do produto em: %s\n\n",
 	    		dataDeFabricacao, dataDeVencimento);
 	    
-	    System.out.println("Produto Vencido ? - " + verificacaoDeVencimento );
+	    System.out.println("Produto Vencido ? - " + date1.isAfter(date2));
 	    
 	    System.out.printf("\nCódigo NCM do produto: %d \n\nCódigo CSOSN da opereação: %d \n\nCódigo CFOP da operação: %d",
 	    		ncm, csosn, cfop); 
